efb0ae47617687ff7ec3e8820690be64032e3a1d
SaiSameekshaVaishnavi <vaishnavi20082005@gmail.com>
2025-05-14 22:17:48 +0530

    Updated CORS Options

--- Backend/config/allowedOrigins.js
+++ Backend/config/allowedOrigins.js
@@ -4,6 +4,7 @@ const allowedOrigins = [
   "https://vercel.com/d-sai-sameeksha-vaishnavis-projects/ale-company/3Mvz4WUbqf18uX8zYMCcTSfh8VmV",
   "https://ale-company-d-sai-sameeksha-vaishnavis-projects.vercel.app/",
   "https://ale-company-git-main-d-sai-sameeksha-vaishnavis-projects.vercel.app/",
+  "https://ale-company.vercel.app/",
 ];
 
 module.exports = allowedOrigins;
