eae71c5522c40f2ced6c631133832a3ee79b39cf
SaiSameekshaVaishnavi <vaishnavi20082005@gmail.com>
2025-05-14 22:27:37 +0530

    Updated

--- Backend/config/allowedOrigins.js
+++ Backend/config/allowedOrigins.js
@@ -1,9 +1,9 @@
 const allowedOrigins = [
   "http://localhost:3500",
   "http://localhost:3000",
-  "https://vercel.com/d-sai-sameeksha-vaishnavis-projects/ale-company/3Mvz4WUbqf18uX8zYMCcTSfh8VmV",
-  "https://ale-company-d-sai-sameeksha-vaishnavis-projects.vercel.app/",
-  "https://ale-company-git-main-d-sai-sameeksha-vaishnavis-projects.vercel.app/",
+  "https://vercel.com",
+  "https://ale-company-d-sai-sameeksha-vaishnavis-projects.vercel.app",
+  "https://ale-company-git-main-d-sai-sameeksha-vaishnavis-projects.vercel.app",
   "https://ale-company.vercel.app",
 ];
 
--- Backend/middleware/credentials.js
+++ Backend/middleware/credentials.js
@@ -2,8 +2,12 @@ const allowedOrigins = require("../config/allowedOrigins");
 
 const credentials = (req, res, next) => {
   const origin = req.headers.origin;
+  console.log("credentials.js: Received Origin:", origin);
   if (allowedOrigins.includes(origin)) {
+    console.log("credentials.js: Origin allowed");
     res.header("Access-Control-Allow-Credentials", true);
+  } else {
+    console.log("credentials.js: Origin NOT allowed");
   }
   next();
 };
