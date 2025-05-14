68995e1e88c6c94609a1256971e7432216a29d4c
SaiSameekshaVaishnavi <vaishnavi20082005@gmail.com>
2025-05-14 22:02:38 +0530

    Updated Backend link

--- frontend/src/routes/refreshToken.js
+++ frontend/src/routes/refreshToken.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an axios instance with a base URL
 const api = axios.create({
-  baseURL: "http://localhost:3500",
+  baseURL: "https://alecompany.onrender.com/",
   withCredentials: true, // Important for sending cookies with refresh tokens
 });
 
